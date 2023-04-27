798df824ba65dbcf4f7eb42f1394f00ac3dd2cfb
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-04-27 16:24:38 -0300

    finish

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
-import { FaPlus } from 'react-icons/fa';
-
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import Form from './Form';
+import Tarefas from './Tarefas';
 
 import './Main.css';
 
@@ -50,6 +48,7 @@ export default class Main extends Component {
       this.setState({
         tarefas: [...novasTarefas],
         index: -1,
+        novaTarefa: '',
       });
     }
   };
@@ -62,6 +61,7 @@ export default class Main extends Component {
 
   handleEdit = (e, index) => {
     const { tarefas } = this.state;
+
     this.setState({
       index,
       novaTarefa: tarefas[index],
@@ -85,31 +85,17 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form onSubmit={this.handleSubmit} action="#" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa} />
-          <button type="submit">
-            <FaPlus />
-          </button>
-        </form>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={tarefa}>
-              {tarefa}
-              <span>
-                <FaEdit
-                  onClick={(e) => this.handleEdit(e, index)}
-                  className="edit"
-                />
-
-                <FaWindowClose
-                  onClick={(e) => this.handleDelete(e, index)}
-                  className="delete"
-                />
-              </span>
-            </li>
-          ))}
-        </ul>
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
       </div>
     );
   }
